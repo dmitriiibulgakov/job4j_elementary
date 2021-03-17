a522cf68d082eae2b29cd6d1ea4cbbef5f47178f
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-03-17 20:16:27 +0700

    5.5. Простое число [#491790]. Рефакторинг.

--- test/ru/job4j/loop/CheckPrimeNumber.java
+++ test/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,8 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number <= 2) {
-            return false;
-        }
+        boolean prime = number > 1;
 
-        boolean prime = true;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 prime = false;
