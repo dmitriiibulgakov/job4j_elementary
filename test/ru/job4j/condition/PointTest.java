c87e96320be62724763d4a38865297eb7abe8dd6
dmitriiibulgakov <dmitriii.bulgakov@gmail.com>
2021-03-25 17:50:06 +0700

    4. Расстояние между точками в трехмерном пространстве. [#491888]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -7,44 +7,54 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(2, 0);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when11to22then1_41() {
         double expected = 1.41;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(2, 2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when01to54then5_83() {
         double expected = 5.83;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 5;
-        int y2 = 4;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(0, 1);
+        Point point2 = new Point(5, 4);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when55to55then0() {
         int expected = 0;
-        int x1 = 5;
-        int y1 = 5;
-        int x2 = 5;
-        int y2 = 5;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point point1 = new Point(5, 5);
+        Point point2 = new Point(5, 5);
+        double out = point1.distance(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000to555then866() {
+        double expected = 8.66;
+        Point point1 = new Point(0, 0, 0);
+        Point point2 = new Point(5, 5, 5);
+        double out = point1.distance3d(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000to000then0() {
+        double expected = 0;
+        Point point1 = new Point(0, 0, 0);
+        Point point2 = new Point(0, 0, 0);
+        double out = point1.distance3d(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
